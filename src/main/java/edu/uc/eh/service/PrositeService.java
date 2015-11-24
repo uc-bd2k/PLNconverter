3d62556d88177320f206c1f3bc8c8905f1180f13
sajmmon <e7UaNvGTqEGL>
2015-11-24 15:56:51 -0500

    Added PSI-MOD Ontology

--- src/main/java/edu/uc/eh/service/PrositeService.java
+++ src/main/java/edu/uc/eh/service/PrositeService.java
@@ -1,12 +1,10 @@
 package edu.uc.eh.service;
 
+import edu.uc.eh.utils.IOUtils;
 import edu.uc.eh.utils.NetworkUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by chojnasm on 11/9/15.
@@ -20,9 +18,10 @@ public class PrositeService {
     public String getTable(String peptide) {
 
         String response = "Prosite API did not respond.";
-        String prositeUrl = String.format("http://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?sig=%s&lineage=9606&db=sp&output=json",peptide);
+        String prositeUrl = String.format("http://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?sig=%s&lineage=9606&db=sp&output=json", peptide);
 
         log.info("Querying: " + prositeUrl);
+        log.info("PsiMod: " + IOUtils.getInstance().readResource("/psi-mod/mapping.csv"));
 
         try {
             response = NetworkUtils.readUrl(prositeUrl);
